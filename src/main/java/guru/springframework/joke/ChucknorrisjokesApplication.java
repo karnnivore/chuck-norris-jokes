78b79407f2846e81d0b6dd8506512ed5909c82d6
Nick Chinsen <nicholaschinsen@gmail.com>
2020-10-05 15:54:49 -0400

    Added Beans.xml and overrided default bean

--- src/main/java/guru/springframework/joke/ChucknorrisjokesApplication.java
+++ src/main/java/guru/springframework/joke/ChucknorrisjokesApplication.java
@@ -2,8 +2,11 @@ package guru.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
+@ImportResource("classpath:Beans.xml")
 public class ChucknorrisjokesApplication {
 
     public static void main(String[] args) {
